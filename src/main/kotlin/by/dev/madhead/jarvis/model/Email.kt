73000d3bb0569bf2048d7b4ad8b0a45024459e1e
madhead <siarhei.krukau@gmail.com>
2017-11-07 03:18:11 +0300

    WIP

--- src/main/kotlin/by/dev/madhead/jarvis/model/Email.kt
+++ src/main/kotlin/by/dev/madhead/jarvis/model/Email.kt
@@ -3,4 +3,7 @@ package by.dev.madhead.jarvis.model
 data class Email(
 		val repo: Repo,
 		val build: Build
-)
+) {
+	val subject: String
+		get() = """${build.status.forHumans}: ${repo.slug}#${build.number} (${if (build.branch.isNullOrBlank()) build.revision else "${build.branch} - ${build.revision}"})"""
+}
